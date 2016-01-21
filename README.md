bd4f2ea4c790fd44938824e0cbde47103adeb2ab
Marisa Kirisame <marisa@sayachan.org>
2016-01-21 20:05:04 +0100

    The first commit is here, let's see how long this lasts before being abandoned.

---
+++ README.md
@@ -0,0 +1,2 @@
+Publicly available sauce codes for my Twitch bot. Have fun with something that
+I haven't even started yet.
---
+++ src/croquette.c
@@ -0,0 +1,12 @@
+/*
+	croquette.c : Croquettebot.
+	Inhale a croquette.
+	(C)2016 Marisa Kirisame, UnSX Team.
+	Released under the GNU GPLv3 (or later).
+*/
+int main( void )
+{
+	/* TODO everything */
+	int puts( const char* );
+	return puts("Inhale a croquette")&0;
+}
